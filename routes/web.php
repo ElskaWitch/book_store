8a8c1ddd2b414b77baa665cf07f0d70908dcee80
ElskaWitch <justinelabas95@gmail.com>
2022-10-25 20:40:57 +0200

    definir $books + route BookController

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+// Route::get('/', function () {
+//     return view('pages.home');
+// });
 
-// Route::get('/', [BookController::class, 'index'])->name('home');
+Route::get('/', [BookController::class, 'index'])->name('home');
 Route::resource('books', BookController::class);
 
 Route::get('/dashboard', function () {
